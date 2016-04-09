5934183fba7373bb927b2cf2d84191d46f253d28
Aleš Musil <aedvin1@gmail.com>
2016-04-09 09:16:24 +0200

    Repaired possible buffer overflow when reading data

--- examples/Rx/Rx.ino
+++ examples/Rx/Rx.ino
@@ -11,30 +11,31 @@ void setup() {
 void loop() {
   if (Serial.available()) { //Any data new data
 
-    uint8_t index = 0; //data buffer index
-    while (Serial.available()) {
-      buffer[index++] = Serial.read();
-    }
-    struct sensor_value sensor;  //sensor structure
-    uint8_t offset = 0;  //offset where our sensor data starts in buffer
+    uint8_t len = Serial.readBytes(buffer, MAX_DATA_LEN);  //read bytes into buffer and store amount of readed data
 
-    while (offset < index) {
-      read_sensor(buffer, index, &sensor, &offset);  //read sensor data from buffer
-      switch (sensor.type) {
-        case SENSOR_TEMP:
-          //we have temperature data in sensor
-          break;
-        case SENSOR_HUMIDITY:
-          //we have humidity data in sensor
-          break;
-        case SENSOR_PRESSURE:
-          //we have pressure data in sensor
-          break;
-        case SENSOR_VOLTAGE:
-          //we have voltage data in sensor
-          break;
+    if (len > 0) {
+
+      struct sensor_value sensor;  //sensor structure
+      uint8_t offset = 0;  //offset where our sensor data starts in buffer
+
+      while (offset < len) {
+        read_sensor(buffer, len, &sensor, &offset);  //read sensor data from buffer
+        switch (sensor.type) {
+          case SENSOR_TEMP:
+            //we have temperature data in sensor
+            break;
+          case SENSOR_HUMIDITY:
+            //we have humidity data in sensor
+            break;
+          case SENSOR_PRESSURE:
+            //we have pressure data in sensor
+            break;
+          case SENSOR_VOLTAGE:
+            //we have voltage data in sensor
+            break;
+        }
+        float value =  sensor.value; //value of our sensor data
       }
-      float value =  sensor.value; //value of our sensor data
     }
   }
   delay(50);
